3e2a998458ede0d3511bdcea873fc3b0a6b57195
JustinMitchell <mitchelljm04@gmail.com>
2023-05-09 16:28:54 -0400

    Home Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,21 @@
+import { Avatar, Box, Typography } from '@mui/material'
 import React from 'react'
+import ParticleBackground from './Particles/ParticleBackground'
 
 const Home = () => {
-  return (
-    <div>Home</div>
+  return ( 
+  
+  
+    <div className='home-page' id='home'>
+      <ParticleBackground />
+      <Box className='home-box'>
+        <Avatar classsName='avatar' sx={{ height: '200px', width: '200px', alignItem: 'center', textAlign: 'center'}} src={'/portfoliophoto.jpg'} />
+        <Typography variant='h4'>
+            Hello! I'm Justin Mitchell
+        </Typography>
+        <Typography>Full-Stack Developer specializing in Frontend Developing</Typography>
+        </Box>
+    </div>
   )
 }
 
