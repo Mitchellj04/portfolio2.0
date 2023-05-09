520b209caf8e73df0f9d8b2706d1b1583d35a104
JustinMitchell <mitchelljm04@gmail.com>
2023-05-09 16:26:09 -0400

    Components Added to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
+
+import Projects from './Components/Projects';
+import Skills from './Components/Skills';
 
 function App() {
+
+
   return (
     <div className="App">
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects/>
+      <Contact />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
