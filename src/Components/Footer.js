5aeae82b07b6efa3cf03c735c07f4859d4160b28
JustinMitchell <mitchelljm04@gmail.com>
2023-05-09 16:38:45 -0400

    Footer Updated with logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Box, IconButton, Typography } from '@mui/material'
+import React, { useEffect } from 'react'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import logo from '../Images/logo-no-background.png'
 
 const Footer = () => {
+
+
   return (
-    <div>Footer</div>
+    <div className='footer'>
+       
+        <img src={logo} className='footer-logo'></img>
+        <Box>
+         <IconButton><GitHubIcon style={{fontSize: 30, color: 'whitesmoke'}}/></IconButton>
+        <IconButton><LinkedInIcon style={{fontSize: 30, color: 'whitesmoke'}}/></IconButton>
+        </Box>
+
+    </div>
   )
 }
 
