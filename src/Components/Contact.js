2a2a630d8177256a57b23ed4d96664f5d94c1a2b
JustinMitchell <mitchelljm04@gmail.com>
2024-01-02 13:54:58 -0500

    Build Fail fix

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,12 @@
-import { Alert, Box, Button, Fade, Grid, TextField, Typography } from '@mui/material'
+import { Alert, Box, Button, Fade, Grid, TextField, Typography, IconButton } from '@mui/material'
 import EmailIcon from "@mui/icons-material/Email";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import emailjs from '@emailjs/browser';
 import React, {useEffect, useRef, useState} from 'react'
 import '../Contact.css'
 import { TroubleshootOutlined } from '@mui/icons-material';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Contact = () => {
 
