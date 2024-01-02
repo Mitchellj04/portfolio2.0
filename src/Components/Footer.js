71a98fa9b5d2ed3133c517f07c6dcc6b94655e64
JustinMitchell <mitchelljm04@gmail.com>
2024-01-02 13:41:11 -0500

    Footer Link Fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import { Box, IconButton, Typography } from '@mui/material'
+import { Box, IconButton, Link, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -12,8 +12,8 @@ const Footer = () => {
        
         <img src={logo} className='footer-logo'></img>
         <Box>
-         <IconButton><GitHubIcon style={{fontSize: 30, color: 'whitesmoke'}}/></IconButton>
-        <IconButton><LinkedInIcon style={{fontSize: 30, color: 'whitesmoke'}}/></IconButton>
+         <IconButton><Link href='https://github.com/Mitchellj04' target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: 30, color: 'whitesmoke'}}/></Link></IconButton>
+        <IconButton><Link href='https://www.linkedin.com/in/justin-mitchell-887a47149/' target="_blank" rel="noopener noreferrer"><LinkedInIcon style={{fontSize: 30, color: 'whitesmoke'}}/></Link></IconButton>
         </Box>
 
     </div>
